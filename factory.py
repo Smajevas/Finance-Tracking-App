ca5f5a1d066a0edb2fca4f9cb28b5226b827f5a8
Smajevas <gokas713@gmail.com>
2026-04-21 16:21:11 +0300

    Update factory.py

--- factory.py
+++ factory.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from datetime import date
-
 from models import Category, CategoryType, FinancialEntry, Income, Expense, Transaction
 
 # Abstract creator
